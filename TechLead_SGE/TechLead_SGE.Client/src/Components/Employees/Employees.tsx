ef92eb707f7137c1b7ca89935558d60c4fb18590
jalejandrosandoval <jsandoval5@udi.edu.co>
2025-04-24 21:34:15 -0500

    Ajuste estilos.

--- TechLead_SGE/TechLead_SGE.Client/src/Components/Employees/Employees.tsx
+++ TechLead_SGE/TechLead_SGE.Client/src/Components/Employees/Employees.tsx
@@ -4,9 +4,15 @@ import "./Employees.css";
 const Employees = () => {
     return (
         <LayoutDefault>
-            <div className='ContainerHome'>
-                <h2>Empleados...</h2>
+            <div className="ContainerHome">
+                <div className="TitleCenter">
+                    <h2>Empleados...</h2>
+                </div>
+                <div className="content-container-emp">
+                    prueba.........
+                </div>
             </div>
+
         </LayoutDefault>
     );
 };
